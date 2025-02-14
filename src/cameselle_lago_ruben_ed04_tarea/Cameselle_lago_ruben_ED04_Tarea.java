a81d459fd9be7a254a73558eddb27784b68511db
Ruben <ruben@email.com>
2025-02-14 22:54:51 +0100

    Primer commit

---
+++ src/cameselle_lago_ruben_ed04_tarea/Cameselle_lago_ruben_ED04_Tarea.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cameselle_lago_ruben_ed04_tarea;
+
+/**
+ *
+ * @author i_djr
+ */
+public class Cameselle_lago_ruben_ED04_Tarea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
